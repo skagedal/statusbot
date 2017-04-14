d977ad96402d58c3ccf4167a372b2f6a61a01de1
Simon Kågedal Reimer <simon.reimer@bontouch.com>
2017-04-14 23:22:19 +0200

    Use an environment variable for the token

--- Sources/Secret-template.swift
+++
@@ -1,5 +0,0 @@
-// Edit this file and rename it Secret.swift.  Don't commit.
-
-enum Secret {
-    static let token = "xoxb-SOME-TOKEN"
-}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,13 @@
 import Foundation
 
-let statusbot = StatusBot(token: Secret.token,
+let env = ProcessInfo.processInfo.environment
+
+guard let token = env["STATUSBOT_SLACK_TOKEN"] else {
+    print("Set environment variable STATUSBOT_SLACK_TOKEN")
+    exit(1)
+}
+
+let statusbot = StatusBot(token: token,
                           channel: "test-your-bot-here")
 
 print("Running Statusbot")
